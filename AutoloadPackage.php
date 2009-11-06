51da49affc9aac81e996d98a011691556e98f5cb
Brett Bieber <bbieber@ucommbieber.unl.edu>
2009-11-06 13:22:13 -0600

    Check for file before installing packages

--- AutoloadPackage.php
+++ AutoloadPackage.php
@@ -9,23 +9,24 @@ class AutoloadPackage
     
     static function autoload($class)
     {
-        $package = self::classToPackage($class);
-        self::getPyrus();
-        $class = str_replace('_', '\\', $class);
-        $class = implode('/', explode('\\', $class));
-        if (file_exists('phar://' . self::$pyrus_file . '/PEAR2_Pyrus-2.0.0a1/php/' . $class . '.php')) {
-            include 'phar://' . self::$pyrus_file . '/PEAR2_Pyrus-2.0.0a1/php/' . $class . '.php';
-            return;
+        $file = str_replace('_', '\\', $class);
+        $file = implode('/', explode('\\', $file));
+        if (!file_exists(self::$autoload_registry . '/php/' . $file . '.php')) {
+            $package = self::classToPackage($class);
+            self::getPyrus();
+            if (file_exists('phar://' . self::$pyrus_file . '/PEAR2_Pyrus-2.0.0a1/php/' . $file . '.php')) {
+                include 'phar://' . self::$pyrus_file . '/PEAR2_Pyrus-2.0.0a1/php/' . $file . '.php';
+                return;
+            }
+            self::installPackage($package);
         }
-        self::installPackage($package);
-        include self::$autoload_registry . '/php/' . $class . '.php';
+        include self::$autoload_registry . '/php/' . $file . '.php';
     }
     
     static function installPackage($package)
     {
         $config = \pear2\Pyrus\Config::singleton(self::$autoload_registry);
         $config->preferred_state = 'alpha';
-
         $p = new \pear2\Pyrus\Package($package);
         try {
             \pear2\Pyrus\Installer::begin();
@@ -52,5 +53,3 @@ class AutoloadPackage
 }
 
 spl_autoload_register('AutoloadPackage::autoload');
-
-?>
